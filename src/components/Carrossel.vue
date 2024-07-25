65254c493f2a75b2a967352662369acb8a68f714
Victor Lima <119141946+VictorLimaS@users.noreply.github.com>
2024-07-25 20:22:21 -0300

    Victor - Subindo o corpo do inicio

---
+++ src/components/Carrossel.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="carrossel" :style="{ width, height }">
+      <transition name="fade" @before-enter="beforeEnter" @enter="enter" @leave="leave">
+        <img
+          v-if="images.length"
+          :key="currentIndex"
+          :src="images[currentIndex]"
+          alt="Carrossel"
+          class="carrossel-image"
+        />
+      </transition>
+      <div class="carrossel-slot">
+        <slot></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Carrossel',
+    props: {
+      width: {
+        type: String,
+        default: '100%'
+      },
+      height: {
+        type: String,
+        default: '100vh'
+      },
+      images: {
+        type: Array,
+        default: () => []
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0,
+        interval: null
+      };
+    },
+    methods: {
+      nextImage() {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      },
+      beforeEnter(el) {
+        el.style.opacity = 0;
+        el.style.transform = 'scale(1.1)';
+      },
+      enter(el, done) {
+        el.offsetHeight; // trigger reflow
+        el.style.transition = 'opacity 1.5s ease-in-out, transform 1.5s ease-in-out';
+        el.style.opacity = 1;
+        el.style.transform = 'scale(1)';
+        done();
+      },
+      leave(el, done) {
+        el.style.transition = 'opacity 2s ease-in-out, transform 2s ease-in-out'; // Slow transition on leave
+        el.style.opacity = 0;
+        el.style.transform = 'scale(1.1)';
+        done();
+      }
+    },
+    mounted() {
+      this.interval = setInterval(this.nextImage, 6000); // Change image every 6 seconds
+    },
+    beforeDestroy() {
+      clearInterval(this.interval);
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .carrossel {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .carrossel-image {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    transition: opacity 1.5s ease-in-out, transform 1.5s ease-in-out;
+  }
+  
+  .carrossel-slot {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    padding: 5px 10px;
+    border-radius: 5px;
+    z-index: 1
+  }
+  
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 1.5s ease-in-out, transform 1.5s ease-in-out;
+  }
+  
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
+  
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,50 @@
 <template>
-    <div class="home">
-        <Theheader/>
-        
+  <div class="home">
+    <Theheader />
+    <div class="carrossel-container">
+      <Carrossel width="100%" height="80vh"
+        :images="['../src/assets/TRIO01.jpg', '../src/assets/TRIO02.jpg', '../src/assets/TRIO03.jpg']">
+        <div class="img">
+          <img class="name" src="../assets/name.png" >
+          <img class="logo" src="../assets/logo.png" >
+        </div>
+      </Carrossel>
     </div>
-  </template>
-  
-  <script>
-  import Theheader from '../components/Theheader.vue';
-  
-  export default {
-    name: 'home',
-    components: {
-        Theheader,
-    }
+  </div>
+</template>
+
+<script>
+import Carrossel from '../components/Carrossel.vue';
+import Theheader from '../components/Theheader.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Theheader,
+    Carrossel
   }
-  
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.carrossel-container {
+  display: flex;
+  align-items: center;
+}
+
+.img {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.img .name {
+  width: 70rem;
+  height: auto;
+}
+
+.img .logo{
+  width: 10rem;
+  height: auto;
+}
+</style>
