0c9c1c93a8ee1d288be2d82cfc29f0cc154b034f
Victor Lima <victorlimasantos99@hotmail.com>
2024-11-30 13:44:37 -0300

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'; // Correct import syntax
 import Home from '../pages/Home.vue';
 import QuemSomos from '../pages/QuemSomos.vue';
 import Cultos from '../pages/Cultos.vue';
@@ -6,16 +7,18 @@ import Dizimo from '../pages/Dizimo.vue';
 import Cursos from '../pages/Cursos.vue';
 import Contato from '../pages/Contato.vue';
 
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/quemSomos', name: 'QuemSomos', component: QuemSomos },
+  { path: '/cultos', name: 'Cultos', component: Cultos },
+  { path: '/dizimo', name: 'Dizimo', component: Dizimo },
+  { path: '/cursos', name: 'Cursos', component: Cursos },
+  { path: '/contato', name: 'Contato', component: Contato },
+];
+
 const router = createRouter({
-  mode: "hash", 
-  routes: [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/quemSomos', name: 'QuemSomos', component: QuemSomos },
-    { path: '/cultos', name: 'Cultos', component: Cultos },
-    { path: '/dizimo', name: 'Dizimo', component: Dizimo },
-    { path: '/cursos', name: 'Cursos', component: Cursos },
-    { path: '/contato', name: 'Contato', component: Contato },
-  ],
+  history: createWebHistory(), // Use createWebHistory for history mode
+  routes
 });
 
 export default router;
